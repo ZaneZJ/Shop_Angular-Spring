8e66cc130a8f7d0575c81123c5c4b77bfcd0bad8
Zane <76492358+ZaneZJ@users.noreply.github.com>
2021-06-07 22:04:54 +0300

    Moving UserController to controller folder

--- shop/src/main/java/com/zane/shop/ShopResourceController.java
+++ shop/src/main/java/com/zane/shop/ShopResourceController.java
@@ -9,43 +9,11 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/shop")
 public class ShopResourceController {
 
-    private final UserService userService;
-
-    public ShopResourceController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @GetMapping("/all")
-    public ResponseEntity<List<User>> getAllUsers() {
-        List<User> users = userService.findAllUsers();
-        return new ResponseEntity<>(users, HttpStatus.OK);
-    }
-
-    @GetMapping("/find/{username}")
-    public ResponseEntity<User> getUsersByUsername(@PathVariable("username") String username) {
-        User user = userService.findByUsername(username);
-        return new ResponseEntity<>(user, HttpStatus.OK);
-    }
-
-    @PostMapping("/add")
-    public ResponseEntity<User> addUser(@RequestBody User username) {
-        User newUser = userService.addUser(username);
-        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
-    }
-
-    @PutMapping("/update")
-    public ResponseEntity<User> updateUser(@RequestBody User username) {
-        User updateUser = userService.updateUser(username);
-        return new ResponseEntity<>(updateUser, HttpStatus.OK);
-    }
-
-    @PutMapping("/delete/{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable("username") String username) {
-        userService.deleteUser(username);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @GetMapping("/")
+    public String index() {
+        return "Greetings!";
     }
 
 }
---
+++ shop/src/main/java/com/zane/shop/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.zane.shop.controller;
+
+import com.zane.shop.model.User;
+import com.zane.shop.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/shop")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.findAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{username}")
+    public ResponseEntity<User> getUsersByUsername(@PathVariable("username") String username) {
+        User user = userService.findByUsername(username);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<User> addUser(@RequestBody User username) {
+        User newUser = userService.addUser(username);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<User> updateUser(@RequestBody User username) {
+        User updateUser = userService.updateUser(username);
+        return new ResponseEntity<>(updateUser, HttpStatus.OK);
+    }
+
+    @PutMapping("/delete/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable("username") String username) {
+        userService.deleteUser(username);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
